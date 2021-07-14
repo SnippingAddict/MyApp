f754d560a49132e3cc6630383b56e664157ceefd
snippingaddict <nikolay-96@hotmail.com>
2021-07-14 23:44:40 +0200

    Additional work on Items_Grid

--- MyApp/MainWindow.xaml.cs
+++ MyApp/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Timers;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Runtime.CompilerServices;
 
 namespace MyApp
 {
@@ -20,8 +22,11 @@ namespace MyApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        private static Timer aTimer;
+
         public MainWindow()
         {
+            //possible three milisecond load after button click for usercontrol switch
             InitializeComponent();
 
             YoutubeUser.Visibility = Visibility.Hidden;
@@ -48,16 +53,24 @@ namespace MyApp
 
         private void Quotes_Click(object sender, RoutedEventArgs e)
         {
-            User_Grid.Visibility = Visibility.Visible;
             QuotesUser.Visibility = Visibility.Visible;
-
-            Items_Grid.Visibility = Visibility.Collapsed;
+            switchGrid();
         }
 
         private void Youtube_Click(object sender, RoutedEventArgs e)
         {
             YoutubeUser.Visibility = Visibility.Visible;
+            switchGrid();
+        }
 
+        private void Music_Click(object sender, RoutedEventArgs e)
+        {
+            MusicUser.Visibility = Visibility.Visible;
+            switchGrid();
+        }
+
+        public void switchGrid()
+        {
             User_Grid.Visibility = Visibility.Visible;
             Items_Grid.Visibility = Visibility.Collapsed;
         }
